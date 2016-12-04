c392cb13916b9b317ea1717451aea33ef9c7c466
Dylan Harness <dharness@uwo.ca>
2016-12-04 00:38:54 -0500

    ..

---
+++ frontend/app/src/components/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import logo from './../assets/logo.svg';
+import './../styles/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2>Welcome to Sqwak</h2>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
