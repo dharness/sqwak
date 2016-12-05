d66395b20b0f8c45670df9860b0fcda25db5d5b4
Dylan Harness <dharness@uwo.ca>
2016-12-04 22:12:50 -0500

    added record.js

--- frontend/app/src/components/Record.js
+++ frontend/app/src/components/Record.js
@@ -2,29 +2,37 @@ import React, { Component } from 'react';
 import sqwakLabsLogo from './../assets/images/sqwak-labs-mobile.svg';
 import recordButtonImg  from './../assets/images/record-button-normal.svg';
 import recordButtonActiveImg from './../assets/images/record-button-active.svg';
-
+import recordingService from './../services/recordingService.js';
 
 class Record extends Component {
   
   constructor(props) {
     super(props);
     this.state = {
-      recordButtonImage: recordButtonImg
+      recordButtonImage: recordButtonImg,
+      isRecording: false
     }
   }
 
   startRecording() {
-    this.setState({
-      recordButtonImage: recordButtonActiveImg 
-    });
+    if (!this.state.isRecording) {
+      recordingService.recordFor(3.5 * 1000).then((buffers) => {
+        console.log(buffers);
+        this.setState({ isRecording: false });
+      });
+      this.setState({ isRecording: true });
+    }
   }
 
+  componentDidMount() {
+    recordingService.init();
+  }
 
   render() {
     return (
       <div>
         <img src={sqwakLabsLogo} className="sqwak-labs-logo" role="presentation"/>
-        <img src={this.state.recordButtonImage} className="sqwak-labs-round-button" role="presentation" onClick={this.startRecording.bind(this)}/>
+        <img src={this.state.isRecording ? recordButtonActiveImg : recordButtonImg} className="sqwak-labs-round-button" role="presentation" onClick={this.startRecording.bind(this)}/>
       </div>
     );
   }
