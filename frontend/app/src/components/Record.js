7d2dcaecec3480b0a060888408cc3ec8ff70b4e6
Dylan Harness <dharness@uwo.ca>
2016-12-04 23:11:22 -0500

    added recording and animation

--- frontend/app/src/components/Record.js
+++ frontend/app/src/components/Record.js
@@ -8,20 +8,39 @@ class Record extends Component {
   
   constructor(props) {
     super(props);
+    this.sampleLength = 3.5;
     this.state = {
       recordButtonImage: recordButtonImg,
-      isRecording: false
+      isRecording: false,
+      timeRemaining: this.sampleLength * 1000
     }
+  }
+
+  tick() {
+    this.setState({timeRemaining: this.state.timeRemaining - 100 });
+  }
+
+  startCountdown() {
+    this.countDownIntervalId = setInterval(() => this.tick(), 100 );
   }
 
   startRecording() {
     if (!this.state.isRecording) {
-      recordingService.recordFor(3.5 * 1000).then((buffers) => {
+      this.startCountdown();
+      recordingService.recordFor(this.sampleLength * 1000).then((buffers) => {
+        clearInterval(this.countDownIntervalId);
         console.log(buffers);
-        this.setState({ isRecording: false });
+        this.setState({ isRecording: false, timeRemaining: this.sampleLength * 1000 });
       });
       this.setState({ isRecording: true });
     }
+  }
+
+  getTime() {
+    const timeString = this.state.timeRemaining.toString();
+    const seconds = (this.state.timeRemaining >= 1000) ? timeString[0] : '0';
+    const milliseconds = timeString[1] + timeString[2]
+    return { seconds, milliseconds }
   }
 
   componentDidMount() {
@@ -33,6 +52,9 @@ class Record extends Component {
       <div>
         <img src={sqwakLabsLogo} className="sqwak-labs-logo" role="presentation"/>
         <img src={this.state.isRecording ? recordButtonActiveImg : recordButtonImg} className="sqwak-labs-round-button" role="presentation" onClick={this.startRecording.bind(this)}/>
+        <div className="sqwak-labs-recording-timer">
+          {this.getTime().seconds}:{this.getTime().milliseconds}
+        </div>
       </div>
     );
   }
