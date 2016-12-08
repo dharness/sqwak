67788b5ce0694af5e6badc30fc0b585494ad2004
Dylan Harness <dharness@uwo.ca>
2016-12-08 00:24:59 -0500

    added deployment scripts

---
+++ deploy.sh
@@ -0,0 +1,3 @@
+scp ./docker-compose.yml root@sqwak.kingofthestack.com:/usr/src/app/docker-compose.yml
+scp ./docker-compose.prod.yml root@sqwak.kingofthestack.com:/usr/src/app/docker-compose.prod.yml
+scp ./docker-compose.override.yml root@sqwak.kingofthestack.com:/usr/src/app/docker-compose.override.yml
