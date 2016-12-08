f2a5c29907ebaa3e33275e3a1ab377639bbdcb6f
Dylan Harness <dharness@uwo.ca>
2016-12-08 18:27:59 -0500

    ..

--- web/app/app.js
+++ web/app/app.js
@@ -32,10 +32,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
   
-app.use('/', index);
-app.use('/sounds', sounds);
-app.use('/original-sqwak', originalSqwak);
-app.use('/labs', labsController);
+app.use('/api/sounds', sounds);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
