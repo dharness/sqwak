3496f76339b4bfce2c2073bb76632b0cbb9b614a
Dylan Harness <dharness@uwo.ca>
2016-12-08 22:48:18 -0500

    added protocol

--- web/app/app.js
+++ web/app/app.js
@@ -28,7 +28,7 @@ app.set('view engine', 'pug');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json({limit: '16mb'}));
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false, limit: '16mb'}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
   
