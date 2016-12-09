c51ecf3b0522360516d7535dd0e5f577c2f32249
Dylan Harness <dharness@uwo.ca>
2016-12-09 01:25:21 -0500

    fixed styles and deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-export REACT_APP_API_URL=localhost
+export REACT_APP_API_URL=sqwak.kingofthestack.com
 (cd ./frontend/app; npm run build)
 rm -rf ./nginx/www
 mkdir ./nginx/www
@@ -7,3 +7,8 @@ cp -r ./frontend/app/build/ ./nginx/www/
 scp ./docker-compose.yml root@sqwak.kingofthestack.com:/usr/src/app/docker-compose.yml
 scp ./docker-compose.prod.yml root@sqwak.kingofthestack.com:/usr/src/app/docker-compose.prod.yml
 scp ./docker-compose.override.yml root@sqwak.kingofthestack.com:/usr/src/app/docker-compose.override.yml
+
+docker-compose build nginx
+docker push sqwak/nginx
+
+ssh root@sqwak.kingofthestack.com "cd /usr/src/app && docker pull sqwak/nginx && docker-compose -f docker-compose.yml -f docker-compose.prod.yml up -d"
