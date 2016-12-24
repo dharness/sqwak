a33216b9c9a38188948e68a5d5d3a4f2b4e6f98d
nnur <23naila.n@gmail.com>
2016-12-24 00:36:30 -0500

    graphed loss function per batch of 10 sqwaks

--- similarity-service/experiments/ex2/model.py
+++ similarity-service/experiments/ex2/model.py
@@ -1,7 +1,9 @@
 import tensorflow as tf
-
+import os
 
 def train(data_set, sample_length):
+    logs_path = os.path.realpath(__file__) + "/../logs"
+
     x = tf.placeholder(tf.float32, [None, sample_length])
     W = tf.Variable(tf.zeros([sample_length, 10]))
     b = tf.Variable(tf.zeros([10]))
@@ -10,15 +12,23 @@ def train(data_set, sample_length):
 
     cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
     train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+
     init = tf.global_variables_initializer()
     sess = tf.Session()
     sess.run(init)
+    
+    tf.summary.scalar('cross_entropy', cross_entropy)
+    merged = tf.summary.merge_all()
+    summary_writer = tf.train.SummaryWriter(logs_path, sess.graph)
 
-    for i in range(1):
-        batch_xs, batch_ys = data_set.train.next_batch(1)
-        sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
+
+    for i in range(1000):
+        batch_xs, batch_ys = data_set.train.next_batch(100)
+        s, train_result = sess.run([merged, train_step], feed_dict={x: batch_xs, y_: batch_ys})
+        summary_writer.add_summary(s, i)
 
     correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
     accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
     result = sess.run(accuracy, feed_dict={x: data_set.test.samples, y_: data_set.test.labels})
+
     return result
--- similarity-service/experiments/ex2/testing.py
+++ similarity-service/experiments/ex2/testing.py
@@ -7,24 +7,25 @@ import tensorflow as tf
 db = MongoClient()
 results = db.sqwaks.sounds.find()
 N = len(results[0]["amplitudes"])
+L = results.count()
 x_data = []
-
+y_data = []
 for sample in results:
     x_data.append(sample["amplitudes"])
+    y_data.append([sample["rating"]])
 
-x_data = np.array(x_data)
-print(x_data.shape)
+x_data = np.array(x_data).astype(np.float32)
+y_data = np.array(y_data)
+#y_data = x_data * 10 + 2
 
-y_data = x_data * 10 + 2
-
-W = tf.Variable(tf.zeros([N]))
-b = tf.Variable(tf.zeros([1]))
-y = W * x_data + b
+W = tf.Variable(tf.zeros([N, 1]))
+b = tf.Variable(tf.zeros([L]))
+y = tf.reduce_sum(tf.matmul(x_data, W), reduction_indices=[1], keep_dims=True) + b
 
 
 # Minimize the mean squared errors.
 loss = tf.reduce_mean(tf.square(y - y_data))
-optimizer = tf.train.GradientDescentOptimizer(0.1)
+optimizer = tf.train.GradientDescentOptimizer(0.3)
 train = optimizer.minimize(loss)
 
 # Before starting, initialize the variables.  We will 'run' this first.
@@ -35,7 +36,9 @@ sess = tf.Session()
 sess.run(init)
 
 # Fit the line.
-for step in range(201):
+for step in range(L):
     sess.run(train)
     if step % 20 == 0:
-        print(step, sess.run(W), sess.run(b))
+        print("step", step)
+        print("the W", sess.run(W))
+        print("the beee", sess.run(b))
