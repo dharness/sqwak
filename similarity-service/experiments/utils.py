3c304cdbf91d92288aad20db80cac212e37c2d07
nnur <23naila.n@gmail.com>
2017-01-01 18:37:53 -0500

    wrote an mfc function

--- similarity-service/experiments/utils.py
+++ similarity-service/experiments/utils.py
@@ -1,3 +1,12 @@
+from python_speech_features import mfcc
+
+def mfc(x_data, sample_rate):
+    mfcc_data = []
+    for amplitudes in x_data:
+        mfcc_feat = mfcc(amplitudes, sample_rate)
+        mfcc_data.append(mfcc_feat[0])
+    return mfcc_data
+
 def calculate_accuracy(actual_ratings, predicted_ratings, threshold=0.1):
     correct_predictions = 0
     for i, predicted in enumerate(predicted_ratings):
